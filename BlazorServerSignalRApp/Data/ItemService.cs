e52a4909c8d092f70e0af310974be1f2858715b7
krishnamohanparuchuri <paruchurikris@gmail.com>
2022-07-27 19:14:19 +0200

    Add project files.

---
+++ BlazorServerSignalRApp/Data/ItemService.cs
@@ -0,0 +1,52 @@
+﻿using BlazorServerSignalRApp.Models;
+
+namespace BlazorServerSignalRApp.Data
+{
+    public class ItemService
+    {
+
+        public List<Item> items = new()
+        {
+            new Item
+            {
+                Id=1,
+                Price=26.66m,
+                Name= "Painting"
+
+            },
+            new Item
+            {
+                Id=2,
+                Price=27.66m,
+                Name= "Electronics"
+
+            },
+            new Item
+            {
+                Id=3,
+                Price=26.66m,
+                Name= "Books"
+
+            }
+            ,
+            new Item
+            {
+                Id=4,
+                Price=16.26m,
+                Name= "Movie tickets"
+
+            }
+        };
+
+        public async Task<List<Item>> GetItems()
+        {
+            return items;
+        }
+
+        public void GetItem(int id, decimal newBid)
+        {
+            items.FirstOrDefault((i) => i.Id == id).Price = newBid;
+        }
+    }
+}
+    
---
+++ BlazorServerSignalRApp/Hubs/AuctionHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorServerSignalRApp.Hubs
+{
+    public class AuctionHub : Hub
+    {
+
+        public async Task SendNewBid(int Id,decimal newBidValue)
+        {
+            await Clients.All.SendAsync("UpdatedBid", Id, newBidValue);
+        }
+    }
+}
